fd669b0c77f3133ddfafe12eca8a9de00dcb0157
Matheod <matheodgit@gmail.com>
2014-05-17 19:58:44 +0200

    Add aArray class

--- Extension/addic8ed.js
+++ Extension/addic8ed.js
@@ -2,6 +2,43 @@ addic8ed = {
 
 }
 
+aArray = {
+get: function(array, key) {
+	array.forEach(function(v) {
+		if(v[0] === key)	return v[1];
+	});
+},
+set: function(array, key, value) {
+	array.forEach(function(v, i, a) {
+		if(v[0] === key)
+		{
+			array[i] = value;
+			return;
+		}
+	});
+	array.push([key, value]);
+},
+remove: function(array, key) {
+	array.forEach(function(v, i, a) {
+		if(v[0] === key)
+		{
+			array.splice(i, 1);
+			return true;
+		}
+	});
+	return false;
+}
+sort: function(array) {
+	array.sort(this.sortFunction);
+},
+sortFunction: function(a,b){
+  if (a[0] < b[0])
+     return -1;
+  if (a[0] > b[0])
+     return 1;
+  return 0;
+}
+}
 
 window.addEventListener('DOMContentLoaded', function() {
 
